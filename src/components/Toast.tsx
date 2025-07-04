0cfe874d70ceceb404607acc7f362d018bacb5ac
Machfudin <machfudin37@gmail.com>
2025-07-04 21:41:53 +0700

    feat: fix import ReactNode type

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState } from 'react';
+import type { ReactNode } from 'react';
 import { IconCheck, IconInfo, IconXmark } from '@/components/Icons';
 
 type ToastType = 'success' | 'error' | 'info';
